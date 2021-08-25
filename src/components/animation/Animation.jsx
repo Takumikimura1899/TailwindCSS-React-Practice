cec3abbc58a141141ef0ff5e84880c21c029db34
Takumikimura141899 <kimura141899@gmail.com>
2021-08-26 06:07:28 +0900

    addColorSky

--- src/components/animation/Animation.jsx
+++ src/components/animation/Animation.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const Animation = () => {
   return (
     <>
-      <div class='border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto'>
+      <div class='border border-sky-300 shadow rounded-md p-4 max-w-sm w-full mx-auto'>
         <div class='animate-pulse flex space-x-4'>
-          <div class='rounded-full bg-blue-400 h-12 w-12'></div>
+          <div class='rounded-full bg-sky-400 h-12 w-12'></div>
           <div class='flex-1 space-y-4 py-1'>
-            <div class='h-4 bg-blue-400 rounded w-3/4'></div>
+            <div class='h-4 bg-sky-400 rounded w-3/4'></div>
             <div class='space-y-2'>
-              <div class='h-4 bg-blue-400 rounded'></div>
-              <div class='h-4 bg-blue-400 rounded w-5/6'></div>
+              <div class='h-4 bg-sky-400 rounded'></div>
+              <div class='h-4 bg-sky-400 rounded w-5/6'></div>
             </div>
           </div>
         </div>
@@ -33,7 +33,7 @@ const Animation = () => {
 
       <svg
         xmlns='http://www.w3.org/2000/svg'
-        className='w-20 animate-spin transition duration-1000'
+        className='w-20 animate-spin'
         fill='none'
         viewBox='0 0 24 24'
         stroke='currentColor'
